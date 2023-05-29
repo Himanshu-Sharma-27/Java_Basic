cef2fe81ac3d639b8619397876c2b45d5853c886
Himanshu-Sharma-27 <134672984+Himanshu-Sharma-27@users.noreply.github.com>
2023-05-29 20:03:25 +0530

    Add files via upload

---
+++ CountDigit.java
@@ -0,0 +1,13 @@
+public class CountDigit{
+    static void countDigit(int num, int count) {
+        if(num==0){
+            System.out.println(count);
+            return;
+        }
+        countDigit(num/10,count+1);
+    }
+    public static void main(String[] args) {
+        countDigit(27487,0);
+        
+    }
+}
